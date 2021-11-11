99d3a6507c2a92513ec7b8668cc1f28018ac409f
Tiago Perdigao <tiago.perdigao@hotmail.com>
2021-11-11 14:18:08 +0100

    before styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   length = 0;
   includeLetters = false;
-  password = '';
   includeNumbers = false;
   includeSymbols = false;
+  password = '';
 
   onChangeLength(target: EventTarget) {
     const value = (<HTMLInputElement>target).value;
@@ -34,9 +34,35 @@ export class AppComponent {
   }
 
   onButtonClick() {
-    console.log(`About to generate a password with the following: Includes letters: ${this.includeLetters}
-    Includes numbers: ${this.includeNumbers}
-    Includes symbols: ${this.includeSymbols}`);
-    this.password = 'MY PASSWORD!!!';
+    // check if the console logs are working
+    // console.log(`About to generate a password with the following: Includes letters: ${this.includeLetters}
+    // Includes numbers: ${this.includeNumbers}
+    // Includes symbols: ${this.includeSymbols}`);
+    // this.password = 'MY PASSWORD!!!';
+
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
   }
 }
